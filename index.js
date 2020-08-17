33edd6a6d465cd2189fac0f8ba746de262fb7898
Dariush Bahri <dazb@alumni.york.ac.uk>
2020-08-17 23:45:13 +0800

    Part 3c stuff

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
-
-const app = express()
-
-app.use(express.static('build'))
-app.use(cors())
-app.use(express.json())
+const Note = require('./models/note')
+const { response } = require('express')
 
 const requestLogger = (request, response, next) => {
   console.log('Method: ', request.method)
@@ -15,6 +12,13 @@ const requestLogger = (request, response, next) => {
   next()
 }
 
+const app = express()
+// For GET requests, will check build folder if any static paths
+// match the request
+app.use(express.static('build'))
+// Content Origin Resource Sharing allowed 
+app.use(cors())
+app.use(express.json())
 app.use(requestLogger)
 
 let notes = [
@@ -40,30 +44,49 @@ let notes = [
 /**
  * Routes
  */
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
+
+app.get('/api/notes', (request, response) => {
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
 })
 
-app.get('/api/notes', (req, res) => {
-  res.json(notes)
-})
-
-app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
-
-  if (note) {
-    response.json(note)  
-  } else {
-    response.status(404).end()
-  }
+app.get('/api/notes/:id', (request, response, next) => {
+  Note
+    .findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
 app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
+  Note
+    .findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
 
-  response.status(204).end()
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body
+  
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+
+  Note
+    .findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
 })
 
 const generateId = () => {
@@ -76,31 +99,44 @@ const generateId = () => {
 app.post('/api/notes', (request, response) => {
   const body = request.body
   
-  if (!body.content) {
+  if (body.content === undefined) {
     return response.status(400).json({
       error: 'content missing'
     })
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
     date: new Date(),
-    id: generateId(),
-  }
+  })
 
-  notes = notes.concat(note)
-
-  response.json(note)
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
 })
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
 
+// handler of requests with unknown endpoint
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const errorHandler = (error, request, response, next) => {
+  console.log(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+// handler of requests with result to errors
+app.use(errorHandler)
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
--- mongoose.js
+++ mongoose.js
@@ -2,41 +2,41 @@ const mongoose = require('mongoose')
 const { response } = require('express')
 
 if (process.argv.length < 3) {
-    console.log('Please give pass')
-    process.exit(1)
+  console.log('Please give pass')
+  process.exit(1)
 }
 
 const password = process.argv[2]
 
 const url = `mongodb+srv://fullstack:${password}` +
-    `@fullstackopen.qyyfr.mongodb.net/note-app`+
-    `?retryWrites=true&w=majority`
+  `@fullstackopen.qyyfr.mongodb.net/note-app` +
+  `?retryWrites=true&w=majority`
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
 
 const noteSchema = new mongoose.Schema({
-    content: String,
-    date: Date,
-    important: Boolean,
+  content: String,
+  date: Date,
+  important: Boolean,
 })
 
 const Note = mongoose.model('Note', noteSchema)
 
-// const note = new Note({
-//     content: 'bum is EASY',
-//     date: new Date(),
-//     important: true,
-// })
-// 
-// note.save().then(result => {
-//     console.log('Note saved!')
-//     console.log('result: ', result)
+const note = new Note({
+  content: 'Hello there',
+  date: new Date(),
+  important: true,
+})
+
+note.save().then(result => {
+  console.log('Note saved!')
+  console.log('result: ', result)
+  mongoose.connection.close()
+})
+
+// Note.find({ important: true }).then(result => {
+//     result.forEach(note => {
+//         console.log(note)
+//     })
 //     mongoose.connection.close()
 // })
-
-Note.find({ important: true }).then(result => {
-    result.forEach(note => {
-        console.log(note)
-    })
-    mongoose.connection.close()
-})
